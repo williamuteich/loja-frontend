4a8ea52e606f8ee048f1161ad8c26c647fcb69e5
william <williamuteich14@gmail.com>
2025-12-19 19:24:56 -0300

    add menu hambuguer no header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,10 +1,11 @@
-import { Component, ChangeDetectionStrategy, inject, signal, HostListener } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject, signal, HostListener, effect, PLATFORM_ID } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
-import { LucideAngularModule, Search, ShoppingBag, User, Menu, Instagram, Facebook } from 'lucide-angular';
-import { NgOptimizedImage } from '@angular/common';
+import { LucideAngularModule, Search, ShoppingBag, User, Menu, Instagram, Facebook, X, Home, Package, Layers, Phone } from 'lucide-angular';
+import { NgOptimizedImage, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [RouterLink, RouterLinkActive, LucideAngularModule, NgOptimizedImage],
   templateUrl: './header.html',
   styleUrl: './header.css',
@@ -12,15 +13,34 @@ import { NgOptimizedImage } from '@angular/common';
 })
 export class Header {
   private router = inject(Router);
+  private platformId = inject(PLATFORM_ID);
 
   protected readonly Search = Search;
   protected readonly ShoppingBag = ShoppingBag;
   protected readonly User = User;
   protected readonly Menu = Menu;
+  protected readonly XIcon = X;
+  protected readonly Home = Home;
+  protected readonly Package = Package;
+  protected readonly Layers = Layers;
+  protected readonly Phone = Phone;
   protected readonly Instagram = Instagram;
   protected readonly Facebook = Facebook;
 
   protected isScrolled = signal(false);
+  protected isMenuOpen = signal(false);
+
+  constructor() {
+    effect(() => {
+      if (!isPlatformBrowser(this.platformId)) return;
+
+      if (this.isMenuOpen()) {
+        document.body.style.overflow = 'hidden';
+      } else {
+        document.body.style.overflow = '';
+      }
+    });
+  }
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
@@ -28,7 +48,16 @@ export class Header {
     this.isScrolled.set(scrollOffset > 50);
   }
 
+  protected toggleMenu() {
+    this.isMenuOpen.update(v => !v);
+  }
+
+  protected closeMenu() {
+    this.isMenuOpen.set(false);
+  }
+
   protected login() {
+    this.closeMenu();
     this.router.navigate(['/login']);
   }
 }
