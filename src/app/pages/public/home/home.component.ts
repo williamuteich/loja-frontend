b678ad86a5d8436bbb48306fa2388a38aced3688
william <williamuteich14@gmail.com>
2025-12-30 06:13:11 -0300

    melhorando carregamento da home e SEO

--- src/app/pages/public/home/home.component.ts
+++ src/app/pages/public/home/home.component.ts
@@ -1,11 +1,13 @@
-import { Component, inject, computed, OnInit } from '@angular/core';
+import { Component, inject, computed, OnInit, effect } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Title, Meta } from '@angular/platform-browser';
 import { BannerComponent } from '../../../components/home/banner/banner.component';
 import { CategoryCarouselComponent } from '../../../components/home/category-carousel/category-carousel.component';
 import { OfferCarouselComponent } from '../../../components/home/offer-carousel/offer-carousel.component';
 import { CategoryProductsCarouselComponent } from '../../../components/home/category-products-carousel/category-products-carousel.component';
 import { CategoryService } from '../../../services/category.service';
 import { ProductService } from '../../../services/product.service';
+import { StoreConfigService } from '../../../services/store-config.service';
 
 @Component({
     selector: 'app-home',
@@ -23,12 +25,33 @@ import { ProductService } from '../../../services/product.service';
 export class HomeComponent implements OnInit {
     private readonly categoryService = inject(CategoryService);
     private readonly productService = inject(ProductService);
+    private readonly storeConfigService = inject(StoreConfigService);
+    private readonly titleService = inject(Title);
+    private readonly metaService = inject(Meta);
 
     protected readonly homeCategories = computed(() =>
         this.categoryService.publicCategories().filter(c => c.isHome)
     );
 
+    constructor() {
+        effect(() => {
+            const config = this.storeConfigService.config();
+            if (config) {
+                if (config.seoTitle) {
+                    this.titleService.setTitle(config.seoTitle);
+                }
+                if (config.seoDescription) {
+                    this.metaService.updateTag({ name: 'description', content: config.seoDescription });
+                }
+                if (config.ogImageUrl) {
+                    this.metaService.updateTag({ property: 'og:image', content: config.ogImageUrl });
+                }
+            }
+        });
+    }
+
     ngOnInit(): void {
+        this.storeConfigService.loadConfigPublic();
         this.categoryService.loadCategoriesPublic();
         this.productService.loadProductsPublic();
     }
